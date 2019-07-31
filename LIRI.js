709b98e54a3d1c5ec8e62b570b89a02c075a848e
Cole Rudolph <crudolph@gmail.com>
2019-07-31 09:48:32 -0400

    node modules and global variables added

--- LIRI.js
+++ LIRI.js
@@ -1,5 +1,13 @@
+// Node Modules & associated required code
 require("dotenv").config();
 var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+var moment = require("moment");
+moment().format();
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-console.log(spotify)
+// Variables pulled from user input
+var cmd = process.argv[2];
+var input = process.argv[3];
