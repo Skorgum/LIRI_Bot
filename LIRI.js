ae8945123489283e9b564c9562209d7601af1462
Cole Rudolph <crudolph@gmail.com>
2019-08-02 00:17:56 -0400

    added more beeps & boops (also finished movie & log functions)

--- LIRI.js
+++ LIRI.js
@@ -1,4 +1,4 @@
-// Node Modules & associated required code
+// Node Modules & associated code
 require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
@@ -43,16 +43,31 @@ switch (cmd) {
         doThis();
         break;
     default:
-        console.log(`\nInvalid command.  Please try again with "concert-this", "spotify-this-song", "movie-this", or "do-what-it-says".`)
+        console.log(`\nInvalid command.  Please try again with "concert-this" [Artist], "spotify-this-song" [Song], "movie-this" [Movie], or "do-what-it-says".`)
+}
+
+// append to log.txt function
+function logThis(data) {
+    fs.appendFile("log.txt", "\r\n\r\n", function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+    fs.appendFile("log.txt", (data), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("\nlog.txt updated!")
+    });
 }
 
 // concert-this command
 function concertThis(bandSearch) {
     if (!bandSearch) {
-        bandSearch = "Tool"
+        bandSearch = "Clutch"
     };
     console.log("\n==========================================================================");
-    console.log("\nBeep Boop!  Now searching for the next "+ bandSearch + " concert.");
+    console.log("d[ o_0 ]b" + "\nBeep Boop!  Now searching for the next "+ bandSearch + " concert.");
     bandSearch = bandSearch.split(" ").join("+");
     var queryUrl = "https://rest.bandsintown.com/artists/" + bandSearch + "/events?app_id=codingbootcamp#";
     // console.log(queryUrl)
@@ -64,9 +79,9 @@ function concertThis(bandSearch) {
             // console.log(concertDate);
             var momentDate = moment().format("L");
             // console.log(momentDate)
-            console.log("\n==========================================================================");
-            console.log("\nVenue: " + concert[0].venue.name + "\nLocation: " + concert[0].venue.city + ", " + concert[0].venue.region + "\nDate : " + momentDate);
-            console.log("\n==========================================================================");
+            let output = "\n==========================================================================" + "\nVenue: " + concert[0].venue.name + "\nLocation: " + concert[0].venue.city + ", " + concert[0].venue.region + "\nDate : " + momentDate + "\n==========================================================================";
+            console.log(output);
+            logThis(output);
         };
     });
 };
@@ -77,16 +92,16 @@ function spotifyThis(songSearch) {
         songSearch = "Amethyst Realm";
     };
     console.log("\n==========================================================================");
-    console.log("\nBeep Boop!  Now searching for " + songSearch);
+    console.log("d[ o_0 ]b" + "\nBeep Boop!  Now searching for " + songSearch);
     spotify.search({ type: "track", query: songSearch}, function(err, data) {
         if (err) {
             console.log("The following error occurred: " + err);
             return;
         } else {
             // console.log(data)
-            console.log("\n==========================================================================");
-            console.log("\nArtist Name: " + data.tracks.items[0].album.artists[0].name + "\nSong Name: " + data.tracks.items[0].name + "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].album.external_urls.spotify)
-            console.log("\n==========================================================================");
+            let output = "\n==========================================================================" + "\nArtist Name: " + data.tracks.items[0].album.artists[0].name + "\nSong Name: " + data.tracks.items[0].name + "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].album.external_urls.spotify +"\n=========================================================================="
+            console.log(output);
+            logThis(output);
         };
     });
 };
@@ -97,11 +112,36 @@ function movieThis(movieSearch) {
         movieSearch = "The Big Lebowski"
     };
     console.log("\n==========================================================================");
-    console.log("\nBeep Boop!  Now searching for movie info about: " + movieSearch);
+    console.log("\nd[ o_0 ]b" + "\nBeep Boop!  Now searching for movie info for: " + movieSearch);
     movieSearch = movieSearch.split(" ").join("+")
-    axios.get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy")
-    .then(function(response) {
-        console.log(response)
-        // TODO:  convert object response into relevant data
+    axios
+        .get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+            // console.log(response.data)
+            let output = "\n==========================================================================" + "\nTitle: " + response.data.Title + "\nYear released: " + response.data.Year + "\nRated : " + response.data.Rated + "\nCountry: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nStarring: " + response.data.Actors + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n=========================================================================="
+            console.log(output)
+            logThis(output)
+        })
+        .catch(function(error) {
+            if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            } else if (error.request) {
+            console.log(error.request);
+            } else {
+            console.log("Error", error.message);
+            }
+            console.log(error.config);
+        })
+}
+
+// do-what-it-says command
+function doThis() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) throw err;
+        command = data.split(",")
+        console.log(command)
+        // TODO:  convert command array to cmd & search
     })
 }
